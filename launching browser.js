0a1d20d58e11d00b4399722a3750dd1a700c2117
Avinash-2 <avinashkl099@gmail.com>
2021-04-20 18:13:28 +0530

    fourth commit

--- launching browser.js
+++ launching browser.js
@@ -3,6 +3,7 @@ const { chromium } = require('playwright');
 (async () => {
   const browser = await chromium.launch({headless:false});
   const page = await context.newPage();
-  page.goto("https://www.google.co.in/")
+  page.goto("https://www.google.co.in/");
+  await page.keyboard.press('enter');
   await browser.close();
 })();
